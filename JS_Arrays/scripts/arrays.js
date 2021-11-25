3b12dac1c89f3762bbdb3b515f24f467588bff7f
Dmitrii <dozhgibesov@gmail.com>
2021-11-25 12:11:28 +0700

    JS Objects 1. Убран атрибут type у тега script 2. Код в файле arrays.js обернут в самовызывающуюся функцию 3. Код отформатирован 4. переименованы переменные в соответствии с их смыслом 5. Функции возвращают значения результат, а не печатают в консоль 6. Убран ToString()

--- JS_Arrays/scripts/arrays.js
+++ JS_Arrays/scripts/arrays.js
@@ -1,57 +1,63 @@
-function printSortedArray(array) {
-    var sortedArray = array.sort(function (a, b) {
-        return b - a
-    });
-    console.log("array sorted in descending order: " + sortedArray.toString());
-}
-
-function printFirst5Element(array) {
-    var first5Elements = array.slice(0, 5);
-    console.log("First five elements in array: " + first5Elements.toString());
-}
-
-function printLast5Element(array) {
-    var last5Elements = array.slice(array.length - 5);
-    console.log("Last five elements in array: " + last5Elements.toString());
-}
-
-function printEvenElementsSum(array) {
-    var evenElementsSum = array.reduce(function (sum, element) {
-        if (element % 2 === 0) {
-            return sum + element;
-        }
-        return sum;
-    }, 0);
-    console.log("Sum of even elements in array: " + evenElementsSum);
-}
-
-function init100ConsecutiveIntegers() {
-    var array = [];
-    for (var i = 1; i <= 100; i++) {
-        array.push(i);
+(function () {
+    function getSortedArray(array) {
+        return array.sort(function (a, b) {
+            return b - a
+        });
     }
-    return array;
-}
 
-var array = [4, 2, 10, 3, 6, 7, 1];
-console.log("Initial array: " + array.toString());
+    function getFirst5Elements(array) {
+        return array.slice(0, 5);
+    }
 
-printSortedArray(array);
-printFirst5Element(array);
-printLast5Element(array);
-printEvenElementsSum(array);
-init100ConsecutiveIntegers();
+    function getLast5Elements(array) {
+        return array.slice(array.length - 5);
+    }
 
-var elements100Array = init100ConsecutiveIntegers();
-console.log(elements100Array);
+    function getEvenElementsSum(array) {
+        return array.reduce(function (sum, element) {
+            if (element % 2 === 0) {
+                return sum + element;
+            }
+            return sum;
+        }, 0);
+    }
 
-elements100Array
-    .filter(function (value) {
-        return value % 2 === 0;
-    })
-    .map(function (value) {
-        return Math.pow(value, 2);
-    })
-    .forEach(function (evenSquareNumber) {
-        console.log(evenSquareNumber);
-    });
+    function init100ConsecutiveIntegers() {
+        var array = [];
+
+        for (var i = 1; i <= 100; i++) {
+            array.push(i);
+        }
+
+        return array;
+    }
+
+    var array = [4, 2, 10, 3, 6, 7, 1];
+    console.log("Initial array: " + array);
+
+    var sortedArray = getSortedArray(array);
+    console.log("Array sorted in descending order: " + sortedArray)
+
+    var first5Elements = getFirst5Elements(array);
+    console.log("First five elements in array: " + first5Elements);
+
+    var last5Elements = getLast5Elements(array);
+    console.log("Last five elements in array: " + last5Elements);
+
+    var evenElementsSum = getEvenElementsSum(array);
+    console.log("Sum of even elements in array: " + evenElementsSum);
+
+    var elements100Array = init100ConsecutiveIntegers();
+    console.log(elements100Array);
+
+    elements100Array
+        .filter(function (value) {
+            return value % 2 === 0;
+        })
+        .map(function (value) {
+            return Math.pow(value, 2);
+        })
+        .forEach(function (evenSquareNumber) {
+            console.log(evenSquareNumber);
+        });
+})();

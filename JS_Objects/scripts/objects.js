8df19bd3ab6c895c9e56cb00806510763bcdcc24
Dmitrii <dozhgibesov@gmail.com>
2021-11-27 19:03:00 +0700

    JS Objects: 1. Упразднен атрибут type в теге script 2. Код обернут в самовызывающуюся функцию 3. Имена функций отформатированы 4. Массив стран объявлен одним литералом 5. maxCitiesQuantity теперь ищется через reduce 6. функция map в getCountriesWithMaxCitiesQuantity выдает результат 7. countryName в getSumPopulation упразднена 8. код отформатирован.

--- JS_Objects/scripts/objects.js
+++ JS_Objects/scripts/objects.js
@@ -1,97 +1,99 @@
-var russia = {
-    name: "Russia",
-    cities: [
+(function () {
+    var countriesList = [{
+        name: "Russia",
+        cities: [
+            {
+                name: "Novosibirsk",
+                population: 1620162
+            },
+            {
+                name: "Perm",
+                population: 1049199
+            },
+            {
+                name: "Tomsk",
+                population: 568508
+            },
+            {
+                name: "Magadan",
+                population: 91797
+            }
+        ]
+    },
         {
-            name: "Novosibirsk",
-            population: 1620162
+            name: "Espania",
+            cities: [
+                {
+                    name: "Sevilla",
+                    population: 704154
+                },
+                {
+                    name: "Valencia",
+                    population: 796549
+                },
+                {
+                    name: "Madrid",
+                    population: 3155360
+                },
+                {
+                    name: "Barcelona",
+                    population: 1593075
+                }
+            ]
         },
         {
-            name: "Perm",
-            population: 1049199
-        },
-        {
-            name: "Tomsk",
-            population: 568508
-        },
-        {
-            name: "Magadan",
-            population: 91797
-        }
-    ]
-};
+            name: "The Republic of the Congo",
+            cities: [
+                {
+                    name: "Bukavu",
+                    population: 241690
+                },
+                {
+                    name: "Bandundu",
+                    population: 133080
+                },
+                {
+                    name: "Boma",
+                    population: 527725
+                }
+            ]
+        }];
 
-var espania = {
-    name: "Espania",
-    cities: [
-        {
-            name: "Sevilla",
-            population: 704154
-        },
-        {
-            name: "Valencia",
-            population: 796549
-        },
-        {
-            name: "Madrid",
-            population: 3155360
-        },
-        {
-            name: "Barcelona",
-            population: 1593075
-        }
-    ]
-};
+    function getCountriesWithMaxCitiesQuantity(countries) {
+        var maxCitiesQuantity = countries.reduce(function (maxCitiesQuantity, country) {
+            if (country.cities.length > maxCitiesQuantity) {
+                maxCitiesQuantity = country.cities.length;
+            }
 
-var kongo = {
-    name: "The Republic of the Congo",
-    cities: [
-        {
-            name: "Bukavu",
-            population: 241690
-        },
-        {
-            name: "Bandundu",
-            population: 133080
-        },
-        {
-            name: "Boma",
-            population: 527725
-        },
-    ]
-};
-
-function PrintCountriesWithMaxCitiesQuantity(countries) {
-    var maxCitiesQuantity = 0;
-
-    countries.forEach(function (countries) {
-        if (countries.cities.length > maxCitiesQuantity) {
-            maxCitiesQuantity = countries.cities.length;
-        }
-    });
-
-    return countries.filter(function (countries) {
-        return countries.cities.length === maxCitiesQuantity;
-    }).map(function (countries) {
-        console.log(countries.name);
-    });
-}
-
-function PrintSumPopulation(countries) {
-    var sumPopulation = {};
-    countries.forEach(function (countries) {
-        var countryName = countries.name;
-        sumPopulation[countryName] = countries.cities.reduce(function (totalPopulation, cities) {
-            return totalPopulation + cities.population;
+            return maxCitiesQuantity;
         }, 0);
-    });
+
+        return countries.filter(function (country) {
+            return country.cities.length === maxCitiesQuantity;
+        }).map(function (country) {
+            return country.name;
+        });
+    }
+
+    function getSumPopulation(countries) {
+        var sumPopulation = {};
+
+        countries.forEach(function (country) {
+            sumPopulation[country.name] = country.cities.reduce(function (totalPopulation, city) {
+                return totalPopulation + city.population;
+            }, 0);
+        });
+
+        return sumPopulation;
+    }
+
+    console.log(countriesList);
+
+    var countriesWithMaxCitiesQuantity = getCountriesWithMaxCitiesQuantity(countriesList);
+    console.log("Countries with max cities quantity: ");
+    console.log(countriesWithMaxCitiesQuantity);
+
+    var sumPopulation = getSumPopulation(countriesList);
+    console.log("Countries in the format \"name - sum population\": ");
     console.log(sumPopulation);
-}
-
-var countries = [russia, kongo, espania];
-console.log(countries);
-
-console.log("Countries with max cities quantity: ");
-PrintCountriesWithMaxCitiesQuantity(countries);
-
-console.log("countries in the format \"name - sum population\": ")
-PrintSumPopulation(countries);
+})();
